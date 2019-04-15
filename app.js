8531c69acad2c4ef2eae9e9f8639d6c71de06479
rocketman <ryomajk@gmail.com>
2019-04-15 15:44:21 +0900

    drawing done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 500;
-canvas.height = 500; //css 사이즈 말고 따로 지정해줘야함
+canvas.height = 500; //css 사이즈 말고 따로 지정해줘야함 pixel modifier
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -13,12 +13,12 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
+    ctx.beginPath(); //마우스가 움직일때마다 call beginPath
+    ctx.moveTo(x, y); //Moves the starting point of a new sub-path to the (x, y) coordinates.
     //마우스 클릭전까지
-    ctx.beginPath();
-    ctx.moveTo(x, y);
   } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    ctx.lineTo(x, y); //Connects the last point in the current sub-path to the specified (x, y) coordinates with a straight line.
+    ctx.stroke(); //Strokes the current sub-paths with the current stroke style.
   }
   // console.log(event); //event를 console.log 로 열어서 보면 event 속성들 볼 수 있음
   // console.log(x, y);
