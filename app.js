c4500c849f9c81aa485824738bf92dc081ffbd5f
rocketman <ryomajk@gmail.com>
2019-04-15 21:21:53 +0900

    bg_done

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
+const DEFAULT_COLOR = "#2c2c2c";
 canvas.width = 500;
 canvas.height = 500; //css 사이즈 말고 따로 지정해줘야함 pixel modifier
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
+ctx.fillStyle = ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
@@ -50,6 +55,30 @@ function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   // console.log(color);
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  // console.log(event.target.value);
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling) {
+    filling = false;
+    mode.innerText = "Canvas Color";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+  console.log(filling);
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, 700, 700);
+  }
 }
 
 if (canvas) {
@@ -57,9 +86,20 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 // console.log(Array.from(colors)); //array로 만들어줌
 Array.from(colors).forEach(colors =>
   colors.addEventListener("click", handleColorClick)
 );
+
+// console.log(range.value);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
